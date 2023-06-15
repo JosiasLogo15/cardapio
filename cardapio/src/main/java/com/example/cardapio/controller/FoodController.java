c0caa7a1da1e9a8f9e21ff9904b67bb02d8cf278
Josias <josiasmaldonado2002@gmail.com>
2023-06-15 12:48:35 -0400

    Criação dos arquivos e configurações do Front-End da aplicação

--- cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
+++ cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -3,6 +3,7 @@ package com.example.cardapio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,12 +21,14 @@ public class FoodController {
 	@Autowired
 	private FoodRepository repository;
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PostMapping
 	public void saveFood(@RequestBody FoodRequestDTO data) {
 		Food foodData = new Food(data);
 		repository.save(foodData);
 		return;
 	}
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping
 	public List<FoodResponseDTO> getAll() {
 	
